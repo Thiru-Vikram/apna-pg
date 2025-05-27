428ad327b0df2e5d3a53f3b356110f8a332128fa
Thiru-Vikram <vtu24408@veltech.edu.in>
2025-05-27 09:03:11 +0530

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import './Footer.css';
+import "./Footer.css";
 
 function Footer() {
-  
   return (
     <footer className="footer">
       <div className="footer-content">
